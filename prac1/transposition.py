1f111cfcfa054d31adaa1d5857812320b67b2080
surajphirke3 <surajphirke3@gmail.com>
2026-01-20 15:50:34 +0530

    added trasnosition with formating

--- prac1/transposition.py
+++ prac1/transposition.py
@@ -1,13 +1,10 @@
 choice = input("Enter E for Encrypt or D for Decrypt: ").upper()
-
 text = input("Enter text: ")
 text = text.replace(" ", "")   # remove spaces
-
 if choice == "E":
     top = ""
     bottom = ""
     flag = 0
-
     for ch in text:
         if flag == 0:
             top += ch
@@ -15,26 +12,19 @@ if choice == "E":
         else:
             bottom += ch
             flag = 0
-
     print("Ciphertext:", top + bottom)
-
 elif choice == "D":
     half = (len(text) + 1) // 2
     top = text[:half]
     bottom = text[half:]
-
     result = ""
     i = 0
-
     while i < len(bottom):
         result += top[i]
         result += bottom[i]
         i += 1
-
     if len(top) > len(bottom):
         result += top[-1]
-
     print("Plaintext:", result)
-
 else:
     print("Invalid choice")
